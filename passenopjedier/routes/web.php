1a4953b4f5ee43a69ef0eb5ba6b0bba2ebfa7133
SeanDevAc <seanwakker.website@gmail.com>
2024-12-12 17:57:23 +0100

    extra routing

--- passenopjedier/routes/web.php
+++ passenopjedier/routes/web.php
@@ -6,3 +6,15 @@ Route::get('/', function () {
     // return view('welcome');
     return "hello world";
 });
+
+Route::get('/authentication', function () {
+    return "Authentication page";
+});
+
+Route::get('/home', function () {
+    return "Home page ";
+});
+
+Route::get('/profile', function () {
+    return "Insight in user profile";
+});
